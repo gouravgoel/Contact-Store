9e562850b8784b22d12d01512cce2129d2ee195e
gouravgoel <121gouravkolkata@gmail.com>
2020-12-27 21:02:50 +0530

    Added client interface

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Navbar from './componenets/layout/Navbar'
+import { Fragment } from 'react'
+import Home from './componenets/pages/Home'
+import About from './componenets/pages/About'
 
-function App() {
+import ContactState from './context/contact/ContactState'
+import './App.css'
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ContactState>
+      <Router>
+        <Fragment className='App'>
+          <Navbar />
+          <Switch>
+            <div className='container'>
+              <Route exact path='/' component={Home} />
+              <Route exact path='/about' component={About} />
+            </div>
+          </Switch>
+        </Fragment>
+      </Router>
+    </ContactState>
+  )
 }
 
-export default App;
+export default App
